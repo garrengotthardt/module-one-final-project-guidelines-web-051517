674b7e506492c625eee272b92f0eb331cd8ed726
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-31 14:13:07 -0400

    bin/cli.rb

---
+++ bin/cli.rb
@@ -0,0 +1,102 @@
+require 'geokit'
+
+class CLI
+
+
+  def welcome
+    "Welcome to NYC Yellow Cab booking!"
+  end
+
+  def app_description
+    "By providing us your desired pickup and dropoff locaitons, we'll provide you with a fare estimate based on historic fare averages for the distance you're traveling. Given this estimate, if you'd like to book a ride, you'll be able to do so directly in the app."
+  end
+
+  def get_user_input
+    gets.chomp
+  end
+
+  def collect_name_and_create_user
+    "First off, please enter your first name:"
+    first_name = get_user_input
+    "Thanks! Now, please enter your last name:"
+    last_name = get_user_input
+    new_user = User.new(first_name, last_name)
+    new_user
+  end
+
+  def get_origin_location
+    "Please enter the address you're leaving from:"
+    origin = get_user_input
+    get_or_create_location_object(origin)
+  end
+
+  def get_destination_location
+    "Please enter your desired destination:"
+    destination = get_user_input
+    get_or_create_location_object(destination)
+  end
+
+  def tell_user_trip_distance_and_estimate(distance, estimate)
+    "Your trip will be a total distance of #{distance} miles and has an estimated cost of $#{estimate}."
+  end
+
+  def book_trip?(current_trip)
+    "Do you want to book this trip? (Y/N)"
+    answer = get_user_input
+    if answer == "Y" || "y"
+      current_trip.trip_taken? = true
+      "Great, your car will be arrivng shortly!"
+    elsif answer == "N" || "n"
+      "Ok, look forward to seeing you next time"
+    else
+      book_trip?
+    end
+  end
+
+
+  def get_or_create_location_object
+    address = get_user_input
+    address_ll_array = get_address_latitude_longitude_array(address)
+    location = Location.find_or_create_by(address: address, latitude: address_ll_array[0], longitude: address_ll_array[1])
+    location
+  end
+
+  def get_geokit_object(address)
+    geokit_object=Geokit::Geocoders::GoogleGeocoder.geocode address
+    geokit_object
+  end
+
+  def get_address_latitude_longitude_array(address)
+    geokit_object = get_geokit_object(address)
+    lat_long_array = geo_kit_object.ll.split(",")
+  end
+
+
+  def get_distance_between_start_and_end(origin_address, destination_address)
+    get_geokit_object(origin_address).distance_to(get_geokit_object(destination_address)
+  end
+
+  def get_all_fares_that_match_distance(distance)
+    all_fares_2015 = RestClient.get('https://data.cityofnewyork.us/resource/2yzn-sicd.json')
+    all_fares_2015_parsed = JSON.parse(all_fares_2015)
+    all_fares_2015_parsed.find_all do |trip_hash|
+      trip_hash["trip_distance"].to_f.round == distance
+    end
+  end
+
+  def average_fare_cost_for_distance(distance)
+    matched_trips_fares = []
+    trips_distance_match = get_all_fares_that_match_distance(distance)
+    trips_distance_match.each do |trip_hash|
+      total_trip_cost_less_tip = (trip_hash["fare_amount"].to_f + trip_hash["imp_surcharge"].to_f + trip_hash["mta_tax"].to_f + trip_hash["tolls_amount"].to_f + trip_hash["extra"].to_f)
+      matched_trips_fares << total_trip_cost_less_tip
+    end
+    average = matched_trips_fares.inject{ |sum, el| sum + el }.to_f / matched_trips_fares.size
+    average
+  end
+
+
+
+
+
+end
