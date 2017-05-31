d253cd3bd0680db597106d0f0372ea27885eff87
github username <github email address>
2017-05-31 13:02:03 -0400

    added_lat_and_long_to_location_table

--- db/migrate/20170530214017_create_locations.rb
+++ db/migrate/20170530214017_create_locations.rb
@@ -3,7 +3,8 @@ class CreateLocations < ActiveRecord::Migration[5.0]
   def change
     create_table :locations do |t|
       t.string :address
-
+      t.integer :latitude
+      t.integer :longitude
     end
   end
 
